03edc6651eb8e910d7f8720e7578308391f97df3
G <gjaldon85@gmail.com>
2024-10-07 10:47:46 +0800

    Vault reset test

--- packages/solana/contracts/tests/instructions.test.ts
+++ packages/solana/contracts/tests/instructions.test.ts
@@ -2,11 +2,15 @@ import * as anchor from '@coral-xyz/anchor'
 import { BN, Program } from '@coral-xyz/anchor'
 import { SolanaVault } from '../target/types/solana_vault'
 // import { TOKEN_PROGRAM_ID, createInitializeMintInstruction, getMintLen } from '@solana/spl-token'
+import { EVENT_SEED } from "@layerzerolabs/lz-solana-sdk-v2";
 import { getLogs } from "@solana-developers/helpers";
-import { ConfirmOptions, Keypair, PublicKey, SystemProgram } from '@solana/web3.js'
+import { Connection, ConfirmOptions, Keypair, SendTransactionError, PublicKey, SystemProgram } from '@solana/web3.js'
 import { assert } from 'chai'
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
 
 const confirmOptions: ConfirmOptions = { maxRetries: 3, commitment: "confirmed" }
+
+const LAYERZERO_ENDPOINT_PROGRAM_ID = new PublicKey('76y77prsiCMvXMjuoZ5VRrhG5qYBrUMYTE5WgHqgjEn6')
 
 describe('solana-vault', () => {
     // Configure the client to use the local cluster.
@@ -35,13 +39,158 @@ describe('solana-vault', () => {
             .signers([wallet.payer])
             .rpc(confirmOptions)
         
-        const logs = await getLogs(provider.connection, tx)
-        console.log(logs)
-        
         const vaultAuthority = await program.account.vaultAuthority.fetch(vaultAuthorityPda)
         assert.equal(vaultAuthority.owner.toString(), wallet.publicKey.toString())
         assert.equal(vaultAuthority.orderDelivery, true)
         assert.equal(vaultAuthority.dstEid, 42)
         assert.ok(vaultAuthority.solChainId.eq(new BN(12)))
+    })
+
+    it('resets vault', async () => {
+        const [vaultAuthorityPda] = PublicKey.findProgramAddressSync(
+            [Buffer.from("VaultAuthority")],
+            program.programId
+        )
+
+        let vaultAuthority
+        try {
+            vaultAuthority = await program.account.vaultAuthority.fetch(vaultAuthorityPda)
+        } catch {
+            await program.methods
+                .initVault({
+                    owner: wallet.publicKey,
+                    orderDelivery: true,
+                    dstEid: 42,
+                    solChainId: new BN(12),
+                })
+                .accounts({
+                    signer: wallet.publicKey,
+                    vaultAuthority: vaultAuthorityPda,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([wallet.payer])
+                .rpc(confirmOptions)
+            vaultAuthority = await program.account.vaultAuthority.fetch(vaultAuthorityPda)
+        }
+        assert.equal(vaultAuthority.orderDelivery, true)
+
+        await program.methods 
+            .resetVault()
+            .accounts({
+                owner: wallet.publicKey,
+                vaultAuthority: vaultAuthorityPda
+            })
+            .rpc();
+
+        // Reinitialize the vault with new data
+        await program.methods
+            .initVault({
+                owner: wallet.publicKey,
+                orderDelivery: false, 
+                dstEid: 43,           
+                solChainId: new BN(13),
+            })
+            .accounts({
+                signer: wallet.publicKey,
+                vaultAuthority: vaultAuthorityPda,
+                systemProgram: SystemProgram.programId,
+            })
+            .rpc();
+    
+        vaultAuthority = await program.account.vaultAuthority.fetch(vaultAuthorityPda);
+        assert.equal(vaultAuthority.orderDelivery, false);
+        assert.equal(vaultAuthority.dstEid, 43);
+        assert.ok(vaultAuthority.solChainId.eq(new BN(13)));
+    })
+
+    it('initializes oapp', async () => {
+        const [oappPda] = PublicKey.findProgramAddressSync(
+            [Buffer.from("OApp")],
+            program.programId
+        )
+        const [lzReceiveTypesPda] = PublicKey.findProgramAddressSync(
+            [Buffer.from("LzReceiveTypes"), oappPda.toBuffer()],
+            program.programId
+        )
+        const usdcMint = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v")
+
+        const [oappRegistryPda] = PublicKey.findProgramAddressSync(
+            [Buffer.from("OApp"), oappPda.toBuffer()],
+            program.programId
+        )
+
+        const [eventAuthorityPda] = PublicKey.findProgramAddressSync(
+            [Buffer.from(EVENT_SEED)],
+            LAYERZERO_ENDPOINT_PROGRAM_ID
+        )
+
+        // const accountInfo = await provider.connection.getAccountInfo(LAYERZERO_ENDPOINT_PROGRAM_ID)
+        // console.log("============ ENDPOINT: ", accountInfo.executable)
+        // console.log(" =============== Program Data: ", accountInfo.data)
+        // const balance = await provider.connection.getBalance(wallet.publicKey);
+        // console.log("Wallet balance:", balance);
+        
+        let tx
+        try {
+            tx = await program.methods
+                    .initOapp({
+                    admin: wallet.publicKey,
+                    endpointProgram: null,
+                    usdcHash: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    usdcMint: usdcMint
+                })
+                .accounts({
+                    payer: wallet.publicKey,
+                    oappConfig: oappPda,
+                    lzReceiveTypes: lzReceiveTypesPda,
+                    systemProgram: SystemProgram.programId,
+                })
+                .remainingAccounts([
+                    {
+                        pubkey: LAYERZERO_ENDPOINT_PROGRAM_ID,
+                        isWritable: true,
+                        isSigner: false,
+                    },
+                    {
+                        pubkey: wallet.publicKey,
+                        isWritable: true,
+                        isSigner: true,
+                    },
+                    {
+                        pubkey: oappPda,
+                        isWritable: false,
+                        isSigner: false,
+                    },
+                    {
+                        pubkey: oappRegistryPda,
+                        isWritable: true,
+                        isSigner: false,
+                    },
+                    {
+                        pubkey: SystemProgram.programId,
+                        isWritable: false,
+                        isSigner: false,
+                    },
+                    {
+                        pubkey: eventAuthorityPda,
+                        isWritable: true,
+                        isSigner: false,
+                    },
+                    {
+                        pubkey: LAYERZERO_ENDPOINT_PROGRAM_ID,
+                        isWritable: true,
+                        isSigner: false,
+                    },
+                ])
+                .signers([wallet.payer])
+                .rpc()        
+            const logs = await getLogs(provider.connection, tx)
+            console.log(logs)
+        } catch (e) {
+            console.log("=================================== ERROR")
+            const logs = await e.getLogs(provider.connection)
+            console.log(logs)
+            console.log(e.transactionError)
+        }
     })
 })
