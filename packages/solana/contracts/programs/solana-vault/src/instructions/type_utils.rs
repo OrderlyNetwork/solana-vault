f9436be557d5c363afc3f8aa1aa1a2cecce3c84a
Zion <mrzz@tuta.io>
2024-11-03 23:04:41 +0100

    fix: add account_id validation

--- packages/solana/contracts/programs/solana-vault/src/instructions/type_utils.rs
+++ packages/solana/contracts/programs/solana-vault/src/instructions/type_utils.rs
@@ -1,3 +1,5 @@
+use solana_program::keccak::{hashv, Hash};
+
 pub fn to_bytes32(bytes: &[u8]) -> [u8; 32] {
     let mut bytes32 = [0u8; 32];
     // add ledding zeros to the bytes
@@ -12,6 +14,24 @@ pub fn bytes32_to_hex(bytes: &[u8; 32]) -> String {
         .collect::<Vec<String>>()
         .join("");
     broker_hash_hex
+}
+
+pub fn validate_account_id(
+    account_id: &[u8; 32],
+    user_address: &[u8; 32],
+    broker_hash: &[u8; 32],
+) -> bool {
+    // let mut hasher = keccak::Hasher::default();
+    // msg!(" {:?}", hashv(&[user_address, broker_hash]));
+    if hashv(&[user_address, broker_hash]) == Hash::new(account_id) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+pub fn get_account_id(user_address: &[u8; 32], broker_hash: &[u8; 32]) -> [u8; 32] {
+    hashv(&[user_address, broker_hash]).to_bytes()
 }
 
 // pub fn vec_to_hex(vec: &Vec<u8>) -> String {
