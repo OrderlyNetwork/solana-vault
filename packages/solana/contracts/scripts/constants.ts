66f058fc427fde687e614fdbee15bdff2b992103
Tony Tang <tonytang@TonydeMacBook-Pro.local>
2025-02-26 14:09:07 +0800

    add new broker jojo

--- packages/solana/contracts/scripts/constants.ts
+++ packages/solana/contracts/scripts/constants.ts
@@ -41,7 +41,7 @@ export const TOKEN_SEED = "Token";
 export const OWNER_SEED = "Owner";
 export const ACCOUNT_LIST_SEED = "AccountList";
 
-export const ENV: "DEV" | "QA" | "STAGING" | "MAIN" = "STAGING";
+export const ENV: "DEV" | "QA" | "STAGING" | "MAIN" = "MAIN";
 export const MOCK_OAPP_PROGRAM_ID = new PublicKey("EFLrsQmcfYTSvVrUiP4qruDhbYBtjbQNAhC6tkLJbBtQ");
 export const DEV_OAPP_PROGRAM_ID = new PublicKey("EYJq9eU4GMRUriUJBgGoZ8YLQBXcWaciXuSsEXE7ieQS");
 export const QA_OAPP_PROGRAM_ID = new PublicKey("5zBjLor7vEraAt4zp2H82sy9MSqFoDnNa1Lx6EYKTYRZ");
@@ -127,7 +127,8 @@ export const STAGING_BROKERS = [
     //"cru",
     //"orbidex",
     //"pump_space",
-    "coin98",
+    //"coin98",
+    "jojo",
     ];
 export const MAIN_BROKERS = [
     // "root",
@@ -203,7 +204,8 @@ export const MAIN_BROKERS = [
     //"cru",
     //"orbidex",
     //"pump_space",
-    "coin98",
+    //"coin98",
+    "jojo",
 ]
 
 
