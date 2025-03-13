e0141f8c055620651d983a9c82ac1b775c1a132a
Zion <mrzz@tuta.io>
2025-03-13 11:36:55 +0100

    sync programid for mainnet

--- packages/solana/contracts/programs/solana-vault/src/lib.rs
+++ packages/solana/contracts/programs/solana-vault/src/lib.rs
@@ -9,7 +9,7 @@ use instructions::*;
 
 use oapp::endpoint::MessagingReceipt;
 
-declare_id!("9shwxWDUNhtwkHocsUAmrNAQfBH2DHh4njdAEdHZZkF2");
+declare_id!("ErBmAD61mGFKvrFNaTJuxoPwqrS8GgtwtqJTJVjFWx9Q");
 
 #[program]
 pub mod solana_vault {
